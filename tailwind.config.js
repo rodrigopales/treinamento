65d1143d4a7d2dda6ea2cdabf3053a81c6c9ecc9
Rodrigo Pales <rtpales@gmail.com>
2021-04-06 14:39:22 -0300

    Testando configurações para aplicar site Franklin

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,16 @@ module.exports = {
   purge: [],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+
+      colors:{
+
+        'blue-main': '#6795a1',
+        'white-main': '#ffffff',
+        'black-main': '#1c1c1c'
+      },
+
+    },
   },
   variants: {
     extend: {},
